6b9dd532d3106f535e520f932bf3329b0c7a592c
quincy-soeliman <qsoeliman.dev@gmail.com>
2015-09-26 20:33:01 +0200

    Placeholder

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Quincy Soeliman</title>
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+	<div class="container">
+		<h1 class="title">Quincy Soeliman</h1>
+	</div>
+</body>
+</html>
